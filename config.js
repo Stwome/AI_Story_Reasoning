3c8a7d80345d0cfe23423845f08b2a2008ac829e
Stwome <steven.w.medinets@gmail.com>
2026-05-09 13:57:25 -0400

    added title comments
    Added comments to describe the purpose of the config file.

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
+/* 
+    File: project2.html
+    Author: Steven Medinets
+    Purpose: This file stores the web address used to connect the webpage to the Cloudflare Worker. 
+*/
 window.STORY_DUEL_CONFIG = {
   apiBaseUrl: "https://story-duel-worker.swmedinets.workers.dev"
 };
